0d66a1760109dffb51856af6866db5fe592017db
Ruben Arcos <ruben.arcos06@gmail.com>
2022-12-17 15:52:57 -0600

    Loops

---
+++ Week3/src/com/campuscoders/objects/Loops.java
@@ -0,0 +1,14 @@
+package com.campuscoders.objects;
+
+public class Loops {
+	public static void main (String[] args) {
+		
+		// initialization, termination condition, increment
+		for (int i = 1; i <= 100; i = i +33) {
+			System.out.print(i + ": ");
+			System.out.println(i*i);
+		}
+		
+		System.out.println("done byotch!!");
+	}
+}
